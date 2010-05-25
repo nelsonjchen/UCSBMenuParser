21126cd6f70449d71c2cf2f93367b098fb66a00a
jomanscool2 <jomanscool2@yahoo.com>
2010-05-24 19:42:25 -0700

    Changed MenuScraperTests slightly

--- tests/com/mindflakes/TeamRED/UCSBScrape/MenuScraperTests.java
+++ tests/com/mindflakes/TeamRED/UCSBScrape/MenuScraperTests.java
@@ -8,15 +8,71 @@ import org.junit.Test;
 public class MenuScraperTests {
 
 	@Test
-	public void makeSureNoExceptions(){
+	public void checkCARRILLO_THIS_WEEK(){
 		try{
 			new UCSBJMenuScraper(new RemoteUCSBMenuFile(RemoteUCSBMenuFile.CARRILLO_THIS_WEEK));
+			assertTrue(true);
+		}catch(Exception e){
+			assertTrue(false);
+		}
+	}	
+	@Test
+	public void checkCARRILLO_NEXT_WEEK(){
+		try{
 			new UCSBJMenuScraper(new RemoteUCSBMenuFile(RemoteUCSBMenuFile.CARRILLO_NEXT_WEEK));
+			assertTrue(true);
+		}catch(Exception e){
+			assertTrue(false);
+		}
+	}
+	@Test
+	public void checkDLG_THIS_WEEK(){
+		try{
 			new UCSBJMenuScraper(new RemoteUCSBMenuFile(RemoteUCSBMenuFile.DLG_THIS_WEEK));
+			assertTrue(true);
+		}catch(Exception e){
+			assertTrue(false);
+		}
+	}
+	@Test
+	public void checkDLG_NEXT_WEEK(){
+		try{
 			new UCSBJMenuScraper(new RemoteUCSBMenuFile(RemoteUCSBMenuFile.DLG_NEXT_WEEK));
+			assertTrue(true);
+		}catch(Exception e){
+			assertTrue(false);
+		}
+	}
+	@Test
+	public void checkORTEGA_THIS_WEEK(){
+		try{
 			new UCSBJMenuScraper(new RemoteUCSBMenuFile(RemoteUCSBMenuFile.ORTEGA_THIS_WEEK));
+			assertTrue(true);
+		}catch(Exception e){
+			assertTrue(false);
+		}
+	}
+	@Test
+	public void checkORTEGA_NEXT_WEEK(){
+		try{
 			new UCSBJMenuScraper(new RemoteUCSBMenuFile(RemoteUCSBMenuFile.ORTEGA_NEXT_WEEK));
+			assertTrue(true);
+		}catch(Exception e){
+			assertTrue(false);
+		}
+	}
+	@Test
+	public void checkPORTOLA_THIS_WEEK(){
+		try{
 			new UCSBJMenuScraper(new RemoteUCSBMenuFile(RemoteUCSBMenuFile.PORTOLA_THIS_WEEK));
+			assertTrue(true);
+		}catch(Exception e){
+			assertTrue(false);
+		}
+	}
+	@Test
+	public void checkPORTOLA_NEXT_WEEK(){
+		try{
 			new UCSBJMenuScraper(new RemoteUCSBMenuFile(RemoteUCSBMenuFile.PORTOLA_NEXT_WEEK));
 			assertTrue(true);
 		}catch(Exception e){
